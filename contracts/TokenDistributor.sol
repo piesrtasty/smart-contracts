663a7ffb30185103122ce052b27fdeda179596ae
Luke Hamilton <lukehamilton@Lukes-MacBook-Pro-2.local>
2021-09-27 18:08:52 -0400

    distributor contract complete

--- contracts/TokenDistributor.sol
+++ contracts/TokenDistributor.sol
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract TokenDistributor {
     address public token;
@@ -11,9 +12,28 @@ contract TokenDistributor {
     // This is a packed array of booleans.
     mapping(uint256 => uint256) private claimedBitMap;
 
+    // This event is triggered whenever a call to #claim succeeds.
+    event Claimed(uint256 index, address account, uint256 amount);
+
     constructor(address token_, bytes32 merkleRoot_) {
         token = token_;
         merkleRoot = merkleRoot_;
+    }
+
+    function isClaimed(uint256 index) public view returns (bool) {
+        uint256 claimedWordIndex = index / 256;
+        uint256 claimedBitIndex = index % 256;
+        uint256 claimedWord = claimedBitMap[claimedWordIndex];
+        uint256 mask = (1 << claimedBitIndex);
+        return claimedWord & mask == mask;
+    }
+
+    function _setClaimed(uint256 index) private {
+        uint256 claimedWordIndex = index / 256;
+        uint256 claimedBitIndex = index % 256;
+        claimedBitMap[claimedWordIndex] =
+            claimedBitMap[claimedWordIndex] |
+            (1 << claimedBitIndex);
     }
 
     function claim(
@@ -22,6 +42,18 @@ contract TokenDistributor {
         uint256 amount,
         bytes32[] calldata merkleProof
     ) external {
-        console.logBytes32(merkleProof[0]);
+        require(!isClaimed(index), "TokenDistributor: Drop already claimed.");
+        // Verify the merkle proof.
+        bytes32 node = keccak256(abi.encodePacked(index, account, amount));
+        require(
+            MerkleProof.verify(merkleProof, merkleRoot, node),
+            "TokenDistributor: Invalid proof."
+        );
+        // Mark it claimed and send the token.
+        _setClaimed(index);
+        require(
+            IERC20(token).transfer(account, amount),
+            "TokenDistributor: Transfer failed."
+        );
     }
 }
