1b656abbaa98c8f559f67f2f31606b66d4a6d229
Luke Hamilton <lukehamilton@Lukes-MacBook-Pro-2.local>
2021-11-18 16:23:06 -0500

    change ownership to ownable, remove on-chain gov for now

--- contracts/PUB.sol
+++ contracts/PUB.sol
@@ -2,62 +2,30 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract PUB is
     Initializable,
     ERC20Upgradeable,
-    ERC20PermitUpgradeable,
-    ERC20VotesUpgradeable,
-    AccessControlUpgradeable,
+    OwnableUpgradeable,
     UUPSUpgradeable
 {
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
-
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
     function initialize() public initializer {
         __ERC20_init("Pub", "PUB");
-        __AccessControl_init();
+        __Ownable_init();
         __UUPSUpgradeable_init();
 
         _mint(msg.sender, 100_000_000 * 10**decimals());
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(UPGRADER_ROLE, msg.sender);
     }
 
     function _authorizeUpgrade(address newImplementation)
         internal
         override
-        onlyRole(UPGRADER_ROLE)
+        onlyOwner
     {}
-
-    // The following functions are overrides required by Solidity.
-
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
-        super._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._burn(account, amount);
-    }
 }
