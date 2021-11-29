fbc96d4ae5f7fdb74d4cb4c179c5d024ef789767
Luke Hamilton <lukehamilton@Lukes-MacBook-Pro-2.local>
2021-11-29 16:35:25 -0500

    remove upgradeability

--- contracts/PUB.sol
+++ contracts/PUB.sol
@@ -1,31 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract PUB is
-    Initializable,
-    ERC20Upgradeable,
-    OwnableUpgradeable,
-    UUPSUpgradeable
-{
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
-
-    function initialize() public initializer {
-        __ERC20_init("Pub", "PUB");
-        __Ownable_init();
-        __UUPSUpgradeable_init();
-
-        _mint(msg.sender, 100_000_000 * 10**decimals());
+contract PUB is ERC20 {
+    constructor(address treasury) ERC20("Pub", "PUB") {
+        _mint(treasury, 100_000_000 * 10**decimals());
     }
-
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        override
-        onlyOwner
-    {}
 }
