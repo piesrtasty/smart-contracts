2195faf6ea9386e3f6aca0e6abb5cfb00b76b47c
Luke Hamilton <lukehamilton@Lukes-MacBook-Pro-2.local>
2021-12-08 12:39:45 -0500

    WIP

--- deploy/00_token.js
+++ deploy/00_token.js
@@ -1,11 +1,6 @@
-const config = require("../config");
-
 module.exports = async ({ getNamedAccounts, deployments, network }) => {
   const { deploy } = deployments;
   const { deployer, treasury } = await getNamedAccounts();
-  const namedAccounts = await getNamedAccounts();
-  console.log("named accounts", namedAccounts);
-  console.log("deployer", deployer);
   await deploy("PUBToken", {
     from: deployer,
     args: [treasury],
