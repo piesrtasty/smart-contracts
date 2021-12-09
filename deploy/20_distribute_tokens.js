a0d11bd06d02a19a5502276a97b2981bed210d71
Luke Hamilton <luke@decrypt.co>
2021-12-09 16:37:57 -0500

    distribution

--- deploy/20_distribute_tokens.js
+++ deploy/20_distribute_tokens.js
@@ -1,24 +1,112 @@
 const config = require("../config");
+const fs = require("fs");
+const path = require("path");
 
 const oneToken = ethers.BigNumber.from(10).pow(18);
 
-module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deployer } = await getNamedAccounts();
+const GROUP_TEAM = "team";
+const GROUP_INVESTOR = "investor";
+
+module.exports = async ({ getNamedAccounts }) => {
+  const { treasury } = await getNamedAccounts();
   const pubToken = await ethers.getContract("PUBToken");
   const tokenDistributor = await ethers.getContract("TokenDistributor");
-  const tokenLockInvestor = await ethers.getContract("TokenLockInvestor");
   const tokenLockTeam = await ethers.getContract("TokenLockTeam");
+  const tokenLockInvestor = await ethers.getContract("TokenLockInvestor");
+
+  const DISTRIBUTION_GROUPS = {
+    [GROUP_TEAM]: {
+      lockContract: tokenLockTeam,
+      file: "team-allocations.csv",
+    },
+    [GROUP_INVESTOR]: {
+      lockContract: tokenLockInvestor,
+      file: "investor-allocations.csv",
+    },
+  };
+
+  const distributeToGroup = async (group) => {
+    const { lockContract, file } = DISTRIBUTION_GROUPS[group];
+    const filePath = path.join(__dirname, `../${file}`);
+    if (fs.existsSync(filePath)) {
+      console.log(`Distributing locked ${group} tokens`);
+      const contents = fs.readFileSync(
+        filePath,
+        { encoding: "utf-8" },
+        (err) => {
+          throw err;
+        }
+      );
+      const lines = contents.split("\n");
+      for (let i = 0; i < lines.length; i++) {
+        const row = lines[i];
+        const columns = row.split(",");
+        const [address, amount] = columns;
+        const validatedAddress = ethers.utils.getAddress(address);
+        const lockedTokens = oneToken.mul(amount);
+        try {
+          await (
+            await lockContract.lock(validatedAddress, lockedTokens)
+          ).wait();
+          console.log(`Locked ${amount} tokens for ${address}`);
+        } catch (e) {
+          console.log(`Error locking ${amount} tokens for ${address} - `, e);
+        }
+      }
+    }
+  };
 
   // Transfer airdrop tokens to the token distributor
-  const baseAirdropTokens = oneToken.mul(config.BASE_AIRDROP_TOKENS);
-  const dcptMigrationTokens = oneToken.mul(config.DCPT_MIGRATION_TOKENS);
-  const airdropTokens = baseAirdropTokens.add(dcptMigrationTokens);
-  await (
-    await pubToken.transfer(tokenDistributor.address, airdropTokens)
-  ).wait();
+  const airdropAmount =
+    parseInt(config.BASE_AIRDROP_TOKENS) +
+    parseInt(config.DCPT_MIGRATION_TOKENS);
+  const airdropTokens = oneToken.mul(airdropAmount);
+  console.log("Distributing airdrop tokens");
+  try {
+    await (
+      await pubToken.transfer(tokenDistributor.address, airdropTokens)
+    ).wait();
+    console.log(
+      `Transferred ${airdropAmount} tokens to TokenDistributor (${tokenDistributor.address})`
+    );
+  } catch (e) {
+    console.log(
+      `Error transferring ${airdropAmount} tokens to TokenDistributor (${tokenDistributor.address})`
+    );
+  }
+
+  // Transfer team tokens to the team tokenLock
+  const teamTokens = oneToken.mul(config.TEAM_TOKENS);
+  await (await pubToken.approve(tokenLockTeam.address, teamTokens)).wait();
+  await distributeToGroup(GROUP_TEAM);
 
   // Transfer investor tokens to the investor tokenLock
-  // Transfer team tokens to the team tokenLock
+  const investortokens = oneToken.mul(config.INVESTOR_TOKENS);
+  await (
+    await pubToken.approve(tokenLockInvestor.address, investortokens)
+  ).wait();
+  await distributeToGroup(GROUP_INVESTOR);
+
+  // Transfer remaining tokens to the treasury
+  const totalSupply = oneToken.mul(config.TOTAL_SUPPLY);
+  const remainingSupply = totalSupply
+    .sub(airdropTokens)
+    .sub(teamTokens)
+    .sub(investortokens);
+  console.log("Distributing remaining tokens to Treasury");
+  try {
+    await (await pubToken.transfer(treasury, remainingSupply)).wait();
+    console.log(
+      `Transferred ${
+        remainingSupply.toString() / Math.pow(10, 18)
+      } tokens to Treasury (${treasury})`
+    );
+  } catch (e) {
+    console.log("e", e);
+    console.log(
+      `Error transferring ${airdropAmount} tokens to Treasury (${treasury})`
+    );
+  }
 };
 module.exports.tags = ["distribute"];
 module.exports.dependencies = [
